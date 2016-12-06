0aeb9df252bdf293a60d098840f2518a942cca1c
shiraishi <shiraishi.hideaki@nifty.co.jp>
2016-12-06 06:42:06 +0000

    wip:not preserve exist information in sub-store

--- bin/test_pulish.js
+++ bin/test_pulish.js
@@ -1,77 +1,65 @@
-const mqtt = require('mqtt');
-const config = require('config');
-const commander = require('commander')
+"use strict";
 
-commander
-  .version('0.0.1')
-  .option('-e --endpoint [endpoint]', 'endpoint')
-  .option('-p --port [port]', 'port')
-  .option('-t --topic [topic]', 'topic')
-  .option('-i --interval [interval]', 'Interval')
-  .option('-b --bool <bool>', 'Bool', /^(true|false)$/i, false)
-  .parse(process.argv);
+const uuid = require("uuid");
+const mqtt = require("mqtt");
+const Fs = require("fs");
 
-const UNIQUE_REQUEST_ID = {
-  EVENT_PING: "ping",
-  EVENT_PONG: "pong",
-}
-
-
-const setting = config.mqtt.default;
-if(commander.endpoint){
-  setting.endpoint = commander.endpoint
-}
-if(commander.port){
-  setting.port = commander.port
-}
-if(commander.topic){
-  setting.topic = commander.topic
-}
-if(commander.interval){
-  setting.interval = commander.interval
-}
-if(!setting.endpoint || !setting.port || !setting.topic){
-  return;
-}
-
-const publishPingMessage = (()=>{
-  let i = 0;
-  return ()=>{
-    const json_data = {
-      from: 0,
-      data:{
-        id: -1,  // All
-        eventType: UNIQUE_REQUEST_ID.EVENT_PONG
-      }
-    }
-    return json_data;
-  };
-})();
-
-const client = mqtt.connect(process.env.BROKER || (setting.endpoint+":"+setting.port),{} );
-
-client.on('connect', ()=>{
-  // subscribe
-  client.subscribe(setting.topic, ()=> {
-    console.log(`subscribing ${setting.topic}`)
-  });
-  client.on("message", (tpc, msg)=>{
-    console.log(client);
-    console.log(`subscribe: topic: ${tpc}, msg: ${msg}`);
-  });
-  console.log(commander['bool']);
-  if(commander.bool==='true'){
-    setInterval(()=>{
-      // publish
-      const msg = publishPingMessage();
-      client.publish(setting.topic, JSON.stringify(msg), ()=>{
-        console.log(`publish: topic: ${setting.topic}, msg: ${JSON.stringify(msg)}`);
-      });
-    }, setting.interval);
-  }
+const options = {};
+const con = mqtt.connect(process.env.BROKER || "mqtt://192.168.99.100:1883", options);
+con.on("connect", ()=>{
+  console.log("on connect");
 });
-client.on("error", (err)=>{
+con.on("error", (err)=>{
   console.log(err.stack);
 });
+const topic = process.env.TOPIC || "$ALL";
 
-// mesage.on
+let id = 0;
+class Pong {
+  constructor(max){
+    this.from = -1; //0-150 
+    this.eventType = Pong.eventTypes[Math.floor(Math.random() * Pong.eventTypes.length)];
+    this.min = 0;
+    this.max = max;
+  }
+  fire(){
+    return new Promise((resolve, reject)=>{
+      const msg = (fr,ev) =>{
+        return JSON.stringify({
+          id: -1,
+          from: fr,
+          data:{
+            eventType: ev
+          }
+        });
+      };
+      let counter = 0;
+      const timer = setInterval(()=>{
+        if(counter++ > this.max){
+          clearInterval(timer);
+          resolve(true);
+        }else{
+          const from = Math.floor(Math.random() * (this.max - this.min) + this.min);
+          const eventType = Pong.eventTypes[Math.floor(Math.random() * Pong.eventTypes.length)];
+          console.log(msg(from,eventType));
+          return con.publish(`${topic}/pong`, msg(from,eventType));
+        }
+      }, 2000);
+    });
+  }
+}
+Pong.eventTypes = ["barup", "button1", "button2", "button3"];
+
+const pongs = [];
+for(let i = 0; i < 50; i++){
+  pongs.push(new Pong(Math.floor(Math.random() * 150)));
+}
+
+Promise.all(pongs.map((pong)=> pong.fire())).then(()=>{
+  process.exit(0);
+}).catch((err)=>{
+  console.log(err);
+  process.exit(-1);
+});
+
+
